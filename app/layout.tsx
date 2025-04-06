b1d4a6626bbeb2848fc7d2641fd642d57d802910
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-04-06 10:55:45 -0500

    connect button error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./global.css";
 import { ReactNode } from "react";
 import "@solana/wallet-adapter-react-ui/styles.css"; // ✅ Importa los estilos de wallet
 import WalletContextProvider from '../components/WalletContextProvider'
+import Header from "../components/Header";
+
 export const metadata = {
     title: `Untitled`,
 };
@@ -11,6 +13,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <html lang="en">
             <body>
                 <WalletContextProvider> {/* ✅ Aquí envolvemos sin dañar */}
+                <Header /> {/* ✅ Aquí va */}
+
                     {children}
                 </WalletContextProvider>
             </body>
