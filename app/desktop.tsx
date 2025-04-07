54bba5f703199f3713937f5bbf0b2bd15f908735
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-04-07 14:01:57 -0500

    fix

--- app/desktop.tsx
+++ app/desktop.tsx
@@ -1,36 +1,31 @@
 "use client";
 
 import type { NextPage } from "next";
-import Image from "next/image";
-import Why from "../components/why";
-import Tokenomics from "../components/tokenomics";
-import SwapModule from "../components/SwapModule";
+import FrameComponent1 from "../components/frame-component1";
 import FrameComponent from "../components/frame-component";
 import Footer from "../components/footer";
-import FrameComponent1 from "../components/frame-component1";
 
 const Desktop: NextPage = () => {
   return (
-    <div className="w-full min-h-screen relative bg-gradient-to-b from-[#a2025c] via-[#1b8bad] to-[#363637] overflow-hidden opacity-99 leading-normal tracking-normal text-left text-base text-miscellaneous-floating-tab-pill-fill font-montserrat">
-      {/* Sección Why */}
-     
-
-      {/* Sección Tokenomics */}
+    <div className="w-full min-h-screen flex flex-col bg-gradient-to-b from-[#a2025c] via-[#1b8bad] to-[#363637] font-montserrat text-miscellaneous-floating-tab-pill-fill">
       
+      {/* Sección Swap */}
+      <div className="relative z-10 py-24 px-4">
+        <FrameComponent1 />
+      </div>
 
-      {/* Módulo de Swap */}
-      <section className="relative z-20 flex justify-center items-center py-20">
-        <SwapModule />
-      </section>
+      {/* Sección Informativa */}
+      <div className="relative z-10 py-24 px-4">
+        <FrameComponent />
+      </div>
 
-      
+      {/* Espacio para separar visualmente */}
+      <div className="h-24 w-full" />
 
-     
-
-     
-
-      {/* Pie de página */}
-      
+      {/* Footer */}
+      <footer className="relative z-10 w-full bg-black text-white py-60 px-4">
+        <Footer />
+      </footer>
     </div>
   );
 };
